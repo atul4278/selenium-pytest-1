aaae1c9c8a176163191a2bfa9dd72bf056ad6ad3
Atul Jadhav <atul4278@gmail.com>
2023-04-03 14:48:05 +0530

    add parallel execution with logs being created for each process node

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from configparser import ConfigParser
 
@@ -10,6 +11,30 @@ from utils.driver_manager import get_driver_object
 def pytest_addoption(parser):
     parser.addoption("--env", action='store', default="DEV", help="environment against which tests to be executed")
     parser.addoption("--browser", action='store', default="chrome", help="browser on which tests to be executed")
+
+
+def pytest_configure(config):
+    worker_id = os.environ.get("PYTEST_XDIST_WORKER")
+    if worker_id is not None:
+        logging.basicConfig(
+            datefmt=config.getini("log_file_date_format"),
+            format=config.getini("log_file_format"),
+            filename=f"logs/tests_{worker_id}.log",
+            level=config.getini("log_file_level"),
+        )
+
+
+def pytest_runtest_logstart(nodeid: str):
+    logging.info("-" * 100)
+    logging.info(nodeid)
+    logging.info("-" * 100)
+
+
+def pytest_report_teststatus(report):
+    if report.when == 'call':
+        logging.info("-" * 100)
+        logging.info(f"{report.outcome.upper()} ==> {report.nodeid}")
+        logging.info("-" * 100)
 
 
 @pytest.fixture(scope="session")
