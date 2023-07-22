93eef3a9e7453c18f89e787869f916fc6bb84dc1
Atul Jadhav <atul4278@gmail.com>
2023-07-22 10:54:42 +0530

    remove webdriver-manager dependency

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -5,24 +5,21 @@ from selenium.webdriver.remote.webdriver import WebDriver
 def get_driver_object(browser: str, headless: bool) -> WebDriver:
     driver = None
     if browser.lower() == 'chrome':
-        from selenium.webdriver.chrome.service import Service
-        from webdriver_manager.chrome import ChromeDriverManager
         from selenium.webdriver.chrome.options import Options
         options = Options()
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors=yes')
+        options.add_argument('--start-maximized')
         if headless:
             options.add_argument('--headless=new')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        driver = webdriver.Chrome(options=options)
     elif browser.lower() == 'firefox':
-        from selenium.webdriver.firefox.service import Service
-        from webdriver_manager.firefox import GeckoDriverManager
         from selenium.webdriver.firefox.options import Options
         options = Options()
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors=yes')
         if headless:
             options.add_argument('--headless')
-        driver = webdriver.Chrome(service=Service(GeckoDriverManager().install()), options=options)
+        driver = webdriver.Firefox(options=options)
 
     return driver
